7bf06cc56f22490b88426217d09f1ef5ec445afb
unknown <Michael Nyahende>
2025-09-13 15:14:55 +0300

    guest house list

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/users/", include("users.urls")),
-    # path('api/guesthouses/', include('guesthouses.urls')),
+    path('api/guesthouses/', include('guesthouses.urls')),
     # path("api/bookings/", include("bookings.urls")),
 ]
 
